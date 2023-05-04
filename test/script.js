69024e06012d760408f53f07fd3a4e4c27aaf4de
holynacll <alexandrecurylima@hotmail.com>
2023-05-04 07:53:15 -0300

    first commit

---
+++ test/script.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene(); // cena
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // camera
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const renderer = new THREE.WebGLRenderer(); // renderizador (canvas element)
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 ); // geometry
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); // material 
+// const cube = new THREE.Mesh( geometry, material ); // malha
+// scene.add( cube );
+
+// camera.position.z = 5;
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, material );
+scene.add( line )
+
+function animate() {
+    requestAnimationFrame( animate );
+    line.rotation.x += 0.01;
+    line.rotation.y += 0.01;
+    line.rotation.z += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
